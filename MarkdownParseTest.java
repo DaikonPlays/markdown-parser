058f52b80f00dc1b242dd5dbb045e3b00e6d0561
DaikonPlays <kevinyan904@gmail.com>
2022-05-19 12:46:35 -0700

    tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,4 +1,11 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.*;
 public class MarkdownParseTest {
 
@@ -7,4 +14,13 @@ public class MarkdownParseTest {
         assertEquals(2, 1 + 1);
     }
 
+    @Test
+    public void testFileTest() throws IOException {
+        Path file = Path.of("test-file.md");
+        String content = Files.readString(file);
+        ArrayList<String> links = MarkdownParse.getLinks(content);       
+        List<String> expectedLinks = List.of("https://something.com","some-thing.html");
+        assertArrayEquals(expectedLinks.toArray(), links.toArray());
+        assertArrayEquals(expectedLinks.toArray(), links.toArray());
+    }
 }
